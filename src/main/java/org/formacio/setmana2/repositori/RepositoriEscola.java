e5a1bda186ffc7dbbc5b2d1615d8e2008002f557
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-05-23 20:25:42 +0200

    feat: Anotar el repositorio

--- src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -2,11 +2,13 @@ package org.formacio.setmana2.repositori;
 
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Repository;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Repository
 public class RepositoriEscola {
 
 	
