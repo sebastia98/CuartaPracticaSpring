263f1e281062d6c73a71d8ac0ca84612cd81e592
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-05-23 13:05:48 +0200

    Build: Construir proyecto mvn

---
+++ src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -0,0 +1,22 @@
+package org.formacio.setmana2.domini;
+
+public class Alumne {
+
+	private String nom;
+	
+	private int edat;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getEdat() {
+		return edat;
+	}
+	public void setEdat(int edat) {
+		this.edat = edat;
+	}
+	
+}
